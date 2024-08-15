3363c7079dfe44f7b152c8d7ceddca5f0c57a347
Bread0288 <binlepain178@gmail.com>
2024-08-15 12:28:14 +0900

    Fix: Training GPT2

--- train_torch.py
+++ train_torch.py
@@ -9,7 +9,6 @@ from dataset import BitSequenceDataset
 from transformers import (
     AutoConfig, 
     AutoModel,
-    AutoModelForPreTraining,
     AutoModelForCausalLM,
     AutoTokenizer,
 )
@@ -17,40 +16,37 @@ import wandb
 import logging
 from tqdm.auto import tqdm
 from transformer_lens.utils import lm_cross_entropy_loss
+from olmo.model import OLMo
 
 class TransformerModel(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.transformer = AutoModelForCausalLM.from_config(config)
-        # self.classifier = nn.Linear(config.hidden_size, 1)
-        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
         # transformer_output.keys(): ['last_hidden_state', 'past_key_values']
         # print(self.transformer(x).keys())
         return self.transformer(x)["logits"]
-        # return self.softmax(transformer_output)
-        # return self.classifier(transformer_output[:, 0, :]).squeeze(-1)
     
 def load_scratch_tokenizer(cfg):
     tokenizer = AutoTokenizer.from_pretrained(cfg.model._name_or_path)
-    logging.info(f"original model's tokenizer: \n{tokenizer}\n{tokenizer.special_tokens_map}")
-    if "gpt2" in cfg.model._name_or_path:
+    logging.info(f"original model's tokenizer: \n{tokenizer}\n{tokenizer.special_tokens_map}\n")
+    if "gpt2" in cfg.model._name_or_path.lower():
         from transformers import GPT2TokenizerFast
         new_tokenizer = GPT2TokenizerFast(vocab_file="./vocab/vocab_GPT2.json", 
                                           merges_file="./vocab/vocab_GPT2.txt", 
                                           special_tokens=tokenizer.special_tokens_map, 
                                           model_max_length=1024)
+    # elif "olmo" in cfg.model._name_or_path.lower():
+    #     from transformers import GPTNeoXTokenizerFast
+    #     # Making an Error! : How to Initiate GPTNeoXTokenizerFast?
+    #     # new_tokenizer = GPTNeoXTokenizerFast()
+    #     logging.info(f"new_tokenizer: {new_tokenizer}")
     else:
         raise NotImplementedError
     
     new_tokenizer.add_bos_token = True
-    
-    # tokenizer = BertTokenizer(vocab_file=None)
-    # tokenizer.add_tokens(new_vocab)
-    
     return new_tokenizer
-    
     
 
 def train(cfg: DictConfig):
@@ -62,7 +58,6 @@ def train(cfg: DictConfig):
         if cfg.train.wandb_project_name is None:
             cfg.train.wandb_project_name = "easy-transformer"
         wandb.init(project=cfg.train.wandb_project_name, entity=cfg.train.wandb_entity, config=dict(cfg))
-    # wandb.init(project=cfg.wandb.project_name, config=cfg)
 
     # Set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -74,14 +69,23 @@ def train(cfg: DictConfig):
     # )
     
     # Initialize Model and Tokenizer from scratch
-    model_config = AutoConfig.from_pretrained(cfg.model._name_or_path, **cfg.model)
-    model = TransformerModel(model_config).to(device)
-    logging.info(model.transformer)
-
+    model_name_lower = cfg.model._name_or_path
+    if "gpt2" in model_name_lower:
+        model_config, unused_kwargs = AutoConfig.from_pretrained(cfg.model._name_or_path, return_unused_kwargs=True, force_download=True, **cfg.model)
+        logging.info(f"Loaded Model Config: {model_config}")
+        logging.info(f"unused arguments: {unused_kwargs}")
+        model = TransformerModel(model_config).to(device)
+        logging.info(model.transformer)
+    # elif "olmo" in model_name_lower:
+    #     model = OLMo(cfg.model.config)
+    #     logging.info(f"Total number of parameters: {model.num_params():,d}")
+    #     logging.info(f"Number of non-embedding parameters: {model.num_params(include_embedding=False):,d}")
+    else:
+        raise NotImplementedError
     tokenizer = load_scratch_tokenizer(cfg)
     
-    logging.info(tokenizer)
-    logging.info(tokenizer.get_vocab())
+    logging.info(f"Modified Tokenizer: {tokenizer}")
+    logging.info(f"Modified Tokenizer's vocab: {tokenizer.get_vocab()}")
     
     # Initialize dataset and dataloader
     dataset = BitSequenceDataset(cfg.dataset.train_length, tokenizer)
@@ -91,23 +95,23 @@ def train(cfg: DictConfig):
     optimizer: optim.Optimizer
     if cfg.optimizer.optimizer_name in ["Adam", "AdamW"]:
         # Weight decay in Adam is implemented badly, so use AdamW instead (see PyTorch AdamW docs)
-        if cfg.train.weight_decay is not None:
+        if cfg.optimizer.weight_decay is not None:
             optimizer = optim.AdamW(
                 model.parameters(), 
-                lr=cfg.train.learning_rate,
-                weight_decay=cfg.train.weight_decay,
+                lr=cfg.optimizer.learning_rate,
+                weight_decay=cfg.optimizer.weight_decay,
             )
         else:
             optimizer = optim.Adam(
                 model.parameters(),
-                lr=cfg.train.learning_rate,
+                lr=cfg.optimizer.learning_rate,
             )
     elif cfg.train.optimizer_name == "SGD":
         optimizer = optim.SGD(
             model.parameters(),
-            lr=cfg.train.lr,
-            weight_decay=(cfg.train.weight_decay if cfg.train.weight_decay is not None else 0.0),
-            momentum=cfg.train.momentum,
+            lr=cfg.optimizer.learning_rate,
+            weight_decay=(cfg.optimizer.weight_decay if cfg.optimizer.weight_decay is not None else 0.0),
+            momentum=cfg.optimizer.momentum,
         )
     else:
         raise ValueError(f"Optimizer {cfg.train.optimizer_name} not supported")
@@ -117,7 +121,8 @@ def train(cfg: DictConfig):
     if cfg.train.warmup_steps > 0:
         scheduler = optim.lr_scheduler.LambdaLR(
             optimizer,
-            lr_lambda=lambda step: min(1.0, step / cfg.train.warmup_steps),
+            # lr_lambda=lambda step: min(1.0, step * cfg.train.warmup_steps),
+            lr_lambda=lambda epoch: 0.95 ** epoch,
         )
 
 
@@ -135,12 +140,9 @@ def train(cfg: DictConfig):
             inputs, labels = batch
             inputs = inputs.to(device)
             labels = labels.to(device)
-            # logging.info(f"input: {inputs}\nshape: {inputs.shape}")
-            # logging.info(f"labels: {labels}")
 
             optimizer.zero_grad()
             outputs = model(inputs)
-            # logging.info(f"outputs: {outputs}\nshape: {outputs.shape}")
             # shape: [batch_size, seq_len]
             loss_per_token = lm_cross_entropy_loss(logits=outputs, tokens=inputs, per_token=True)
             avg_loss = loss_per_token[:,-1].mean()
@@ -160,14 +162,14 @@ def train(cfg: DictConfig):
 
             # Calculate & Save accuracy
             prediction_probs = F.softmax(outputs[:, -2, :], dim=-1)
-            # print(f"prediction_probs: {prediction_probs}")
-
-            correct_preds = torch.count_nonzero(torch.eq(labels, torch.argmax(prediction_probs, dim=-1))).item()
-            print(f"correct_preds: {correct_preds}")
+            zero_token_ids, one_token_ids = tokenizer.get_vocab()["0"], tokenizer.get_vocab()["1"]
+            correct_preds = torch.count_nonzero(torch.eq(labels, torch.argmax(torch.index_select(prediction_probs, -1, torch.tensor([zero_token_ids, one_token_ids]).to(device)), dim=-1))).item()
             epoch_correct_preds += correct_preds
             total_correct_preds += correct_preds
-            
-            
+            wandb.log({
+                "learning_rate": scheduler.get_last_lr()[0]  # Retrieve the current learning rate
+            })
+
         wandb.log({
             "epoch": epoch + 1,
             "epoch_avg_loss": epoch_loss / len(dataloader),
@@ -176,7 +178,7 @@ def train(cfg: DictConfig):
             "total_avg_correct_preds": (total_correct_preds / (len(dataset) * (epoch+1))) * 100
         })
 
-        print(f"Epoch {epoch+1}/{cfg.train.num_epochs}, Loss: {epoch_loss / len(dataloader):.4f}, Accuracy: {epoch_correct_preds:.4f}")
+        logging.info(f"Epoch {epoch+1}/{cfg.train.num_epochs}, Loss: {epoch_loss / len(dataloader):.4f}, Accuracy: {epoch_correct_preds:.4f}")
 
     # Save the model
     # torch.save(model.state_dict(), "trained_model.pth")
@@ -188,7 +190,7 @@ def train(cfg: DictConfig):
 
 @hydra.main(version_base=None, config_path="conf", config_name="config_torch")
 def main(cfg: DictConfig):
-    logging.info(f"config: \n{cfg}")
+    logging.info(cfg)
     train(cfg)
 
 if __name__ == "__main__":
